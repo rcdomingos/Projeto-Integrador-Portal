b521c65fcdc3c93678aa84f25745b5f5debad545
Reginaldo C Domingos <rcdomingos@live.com>
2019-05-16 19:46:29 -0300

    add editorconfig e alterado o title das pages

--- imports/head.php
+++ imports/head.php
@@ -1,6 +1,11 @@
+<?php
+//tag<title>  Titulo das paginas;
+$titlePadrao = array('PT' => "O Portal da Música", 'EN' => "The Music Portal", 'ES' => "El Portal de la Música");
+?>
+
 <head>
   <meta charset="utf-8">
-  <title>Ecletic.Music | O Portal da Musica</title>
+  <title>Ecletic.Music | <?php echo (!isset($titlePagina)) ? $titlePadrao[$ID] : $titlePagina[$ID] ?></title>
   <meta name="description" content="Portal de Musica-Projeto integrador">
   <meta name="author" content="Karina, Rafael, Reginaldo, Renata, Thais, Victor - Os Ursal">
 
@@ -14,7 +19,7 @@
     // echo $paginaSocial;
   } else if (
     $pagina == "sertanejo" or $pagina == "pop" or $pagina == "indie" or
-    $pagina == "punk" or $pagina == "classica" or $pagina == "hip-hop"
+    $pagina == "punk" or $pagina == "classica" or $pagina == "hip-hop"  or $pagina == "jazz"
   ) {
     include('../imports/meta-social-pgs.php');
   } else {
@@ -36,7 +41,7 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <?php
   //PHP para ajustar o path do diretorio dos CSS e outros links
-  if ($pagina == "sertanejo" or $pagina == "pop" or $pagina == "indie" or $pagina == "punk" or $pagina == "classica" or $pagina == "hip-hop") {
+  if ($pagina == "sertanejo" or $pagina == "pop" or $pagina == "indie" or $pagina == "punk" or $pagina == "classica" or $pagina == "hip-hop" or $pagina == "jazz") {
     echo '<link rel="stylesheet" href="../css/bootstrap.min.css">' . "\n";
     echo '<link rel="stylesheet" href="../css/mystyle.css">' . "\n";
     echo '<link rel="stylesheet" href="../css/magnific-popup.css">' . "\n";
