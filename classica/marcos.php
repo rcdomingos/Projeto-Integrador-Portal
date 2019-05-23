fba922878e7a77a80befc0225611b13691efe913
Karina Lucindo <klucindo51@gmail.com>
2019-05-23 15:31:42 -0300

    Ajuste na página marcos da seção clássica

--- classica/marcos.php
+++ classica/marcos.php
@@ -30,20 +30,20 @@
     <main id="content">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-7 mt-4">
+                <div class="col-sm-12 col-md-7 col-lg-7 mt-4">
                     <figure>
                         <img src="../images/classica/marcos-01.jpg" class="img-fluid imgmarcosclassica rounded" tabindex="0" alt="<?php echo $classica[$ID][0]?>">
                     </figure>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-5 mt-4">
+                <div class="col-sm-12 col-md-5 col-lg-5 mt-4">
                     <article>
-                        <h2 class="mt-5 mb-5 pt-5 pl-5" tabindex="0"><strong><?php echo $classica[$ID][1]?></strong></h2>
-                        <p class="pl-5" tabindex="0"><strong>SED MALESUADA NUNC DUI ARGUE</strong></p>
-                        <p class="pl-5 mb-5" tabindex="0">Duis augue ante, semper quis lectus eget, iaculis molestie arcu. Quisque efficitur ligula bibendum tempor interdum. Vivamus feugiat, urna in congue fringilla, leo mi posuere enim, ac commodo enim dui sed nulla.</p>
+                        <h2 class="mt-5 mb-5 pt-5" tabindex="0"><strong><?php echo $classica[$ID][1]?></strong></h2>
+                        <p tabindex="0"><strong>SED MALESUADA NUNC DUI ARGUE</strong></p>
+                        <p class="mb-5" tabindex="0">Duis augue ante, semper quis lectus eget, iaculis molestie arcu. Quisque efficitur ligula bibendum tempor interdum. Vivamus feugiat, urna in congue fringilla, leo mi posuere enim, ac commodo enim dui sed nulla.</p>
                     </article>
                     <section>
-                        <h6 class="pl-5" tabindex="0"><strong>AC COMMODO ENIN DUI SED NULLA</strong></h6>
-                        <p class="pl-5" tabindex="0">Phasellus vehicula leo eget lectus pharetra elementum. Etiam leo nisl, commodo non lobortis non, semper id libero. Proin dictum iaculis orci et commodo. Phasellus quis dictum dolor, et dignissim erat.</p>
+                        <h6 tabindex="0"><strong>AC COMMODO ENIN DUI SED NULLA</strong></h6>
+                        <p tabindex="0">Phasellus vehicula leo eget lectus pharetra elementum. Etiam leo nisl, commodo non lobortis non, semper id libero. Proin dictum iaculis orci et commodo. Phasellus quis dictum dolor, et dignissim erat.</p>
                     </section>
                 </div>
             </div>
@@ -80,13 +80,19 @@
                         <p class="text-muted mb-5" tabindex="0">Nam auctor lacus non lorem malesuada, et porta lorem commodo non lobortis non, semper id libero. Proin dictum iaculis orci et commodo. Phasellus quis dictum dolor, et dignissim erat.</p>
                     </article>
                 </div>
+            </div>
+            <div class="row">
                 <div class="col-10 p-auto m-auto">
                     <figure>
                         <img src="../images/classica/marcos-02.jpg" class="img-fluid rounded" tabindex="0" alt="<?php echo $classica[$ID][7]?>">
                     </figure>
                 </div>
-                <div class="mt-3 mb-5">
-                    <p tabindex="0">Maecenas suscipit posuere neque id ultrices. Nulla dui diam, ullamcorper quis nisi a, consectetur tempus eros. Fusce sit amet vehicula ex, quis consectetur lectus. Sed condimentum vel urna et auctor. Cras cursus tortor pretium eros viverra congue. Suspendisse scelerisque leo nisl, id tempus magna posuere a. Nam auctor lacus non lorem malesuada, et porta lorem pharetra. Vestibulum porta odio justo, vel imperdiet nisi suscipit vitae. Nullam dictum turpis a eleifend pellentesque. Vivamus finibus diam ut nibh feugiat imperdiet eget non dolor. Maecenas facilisis elementum convallis. Nulla erat tortor, vestibulum eu iaculis vel, condimentum vel orci. Praesent feugiat rhoncus nisi, in porta odio congue vel. Maecenas laoreet pellentesque eros ut condimentum. Donec justo orci, rutrum nec tellus at, bibendum interdum ex.</p>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="mt-3 mb-5">
+                        <p tabindex="0">Maecenas suscipit posuere neque id ultrices. Nulla dui diam, ullamcorper quis nisi a, consectetur tempus eros. Fusce sit amet vehicula ex, quis consectetur lectus. Sed condimentum vel urna et auctor. Cras cursus tortor pretium eros viverra congue. Suspendisse scelerisque leo nisl, id tempus magna posuere a. Nam auctor lacus non lorem malesuada, et porta lorem pharetra. Vestibulum porta odio justo, vel imperdiet nisi suscipit vitae. Nullam dictum turpis a eleifend pellentesque. Vivamus finibus diam ut nibh feugiat imperdiet eget non dolor. Maecenas facilisis elementum convallis. Nulla erat tortor, vestibulum eu iaculis vel, condimentum vel orci. Praesent feugiat rhoncus nisi, in porta odio congue vel. Maecenas laoreet pellentesque eros ut condimentum. Donec justo orci, rutrum nec tellus at, bibendum interdum ex.</p>
+                    </div>
                 </div>
             </div>
         </div>
